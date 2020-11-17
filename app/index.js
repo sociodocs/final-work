bc029ff612181146d7a785af5094132d1e798632
utkarshbelkhede <utkarshbelkhede@gmail.com>
2020-11-17 14:05:40 +0530

    added login page

--- app/index.js
+++ app/index.js
@@ -38,3 +38,25 @@ function navCheck(entries){
 sections.forEach(section => {
     observer.observe(section);    
 });
+
+// this is used for sliding panel
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('popup');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove('right-panel-active');
+});
+
+// this toggles the blur effect
+function toggle(){
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('active');
+  
+  var popup = document.getElementById('popup');
+  popup.classList.toggle('active');
+}
