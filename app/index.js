ef3983d2ac07492993e546686c799badad24d8cc
utkarshbelkhede <utkarshbelkhede@gmail.com>
2020-11-18 11:52:59 +0530

    validation conatct and donation

--- app/index.js
+++ app/index.js
@@ -60,3 +60,132 @@ function toggle(){
   var popup = document.getElementById('popup');
   popup.classList.toggle('active');
 }
+
+
+//Validate form
+
+function ValidateEmail(email){
+
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)){
+        return (true)
+    }
+    else{
+        return (false)  
+    }   
+}
+
+function ValidateName(name){
+
+    if (/^[a-zA-Z ]{2,15}$/.test(name.value)){
+        return (true)
+    }
+    else{
+        return (false)  
+    }
+}
+
+function ValidateMobile(mobile){
+
+    if (/^\d{10}$/.test(mobile.value)){
+        return (true)
+    }
+    else{
+        return (false)  
+    }
+}
+
+function ValidatePAN(PAN){
+
+    if (/[A-Z]{5}[0-9]{4}[A-Z]{1}/.test(PAN.value)){
+        return (true)
+    }
+    else{
+        return (false)  
+    }
+}
+
+//document.getElementById("d-fn").placeholder = "Type name here..";
+
+//donation form validation
+
+const donationForm = document.getElementById('donation-form-submit');
+
+donationForm.addEventListener('submit', (e) => {
+    
+    var d_fn = document.getElementById('d-fn');
+    var d_ln = document.getElementById('d-ln');
+    var d_em = document.getElementById('d-em');
+    var d_mb = document.getElementById('d-mb');
+    var d_pan = document.getElementById('d-pan');
+
+    if(!ValidateName(d_fn)){
+        d_fn.value = '';
+        d_fn.placeholder = "Invalid Name";
+        d_fn.style.outline = "1px solid red";
+        e.preventDefault();
+    }else
+        d_fn.style.outline = "none";
+
+    if(!ValidateName(d_ln)){
+        d_ln.value = '';
+        d_ln.placeholder = "Invalid Name";
+        d_ln.style.outline = "1px solid red";
+        e.preventDefault();
+    }else
+        d_ln.style.outline = "none";
+
+    if(!ValidateEmail(d_em)){
+        d_em.value = '';
+        d_em.placeholder = "Invaild Email";
+        d_em.style.outline = "1px solid red";
+        e.preventDefault();
+    }else
+        d_em.style.outline = "none";
+
+    if(!ValidateMobile(d_mb)){
+        d_mb.value = '';
+        d_mb.placeholder = "Invaild Mobile";
+        d_mb.style.outline = "1px solid red";
+        e.preventDefault();
+    }else
+        d_mb.style.outline = "none";
+    
+    if(!ValidatePAN(d_pan)){
+        d_pan.value = '';
+        d_pan.placeholder = "Invaild PAN";
+        d_pan.style.outline = "1px solid red";
+        e.preventDefault();
+    }else
+        d_pan.style.outline = "none";
+});
+
+//conatct from validation to
+
+
+
+//success toggle
+
+const success_button = document.getElementById("success-button");
+
+function success_toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    
+    var popup = document.getElementById('success-popup');
+    popup.classList.toggle('active');
+}
+
+function validate_newsletter(email){
+
+    if(ValidateEmail(email)){
+        document.getElementById("success-popup-msg").innerHTML = "SUBSCRIBED";
+        document.getElementById("success-button").innerHTML = "Okay";
+        success_button.style.background = "green";
+    }
+    else{
+        document.getElementById("success-popup-msg").innerHTML = "Not Valid Email!";
+        document.getElementById("success-button").innerHTML = "Try Again";
+        success_button.style.background = "red";
+    }
+    success_toggle();
+}
