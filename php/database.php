52808b7580aaa14145b1883172c855760f04e45d
Aditya Karale <akarale61@gmail.com>
2020-11-27 21:03:19 +0530

    done the function for database.php for connection

--- php/database.php
+++ php/database.php
@@ -1,8 +1,11 @@
 <?php
-	/*
-	$conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1998")
-	or die("Could not connect to server\n");
-	*/
+    $conn ="";
+	if($conn){
 	$conn=pg_connect("host=localhost dbname=venom user=utkarsh port=5432 password=264064")
 	or die("Could not connect to server\n");
+	}
+	else{		
+	$conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1998")
+	or die("Could not connect to server\n");
+	}
 ?>
--- sql/test.sql
+++ sql/test.sql
@@ -1,3 +1,4 @@
 select * from users;
 select * from profile;
 select * from newsletter;
+select * from donation;
